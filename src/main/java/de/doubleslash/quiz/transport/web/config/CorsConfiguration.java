906418129d1814edb6c2e08e8d1edb7d95563426
mpucknat <marvin.pucknat@doubleslash.de>
2023-03-21 10:46:25 +0100

    - Added cors configuration - Parameterized configuration for ci pipeline

--- src/main/java/de/doubleslash/quiz/transport/web/config/CorsConfiguration.java
+++ src/main/java/de/doubleslash/quiz/transport/web/config/CorsConfiguration.java
@@ -1,6 +1,8 @@
 package de.doubleslash.quiz.transport.web.config;
 
 import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -9,12 +11,14 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfiguration {
 
+  @Value("${cors.client.url}")
+  private String allowedOrigin;
   @Bean
   public CorsFilter corsFilter() {
     final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     org.springframework.web.cors.CorsConfiguration config = new org.springframework.web.cors.CorsConfiguration();
     config.setAllowCredentials(true);
-    config.addAllowedOriginPattern("*");
+    config.addAllowedOrigin(allowedOrigin);
     config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "responseType", "Authorization"));
     config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
     source.registerCorsConfiguration("/**", config);
