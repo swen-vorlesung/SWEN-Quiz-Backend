882f87e6de87a4b50489b8e3b49d3c264a70876f
mpucknat <marvin.pucknat@doubleslash.de>
2023-04-03 10:54:51 +0200

    Refactored CORS Code

--- src/main/java/de/doubleslash/quiz/transport/web/config/CorsConfiguration.java
+++ src/main/java/de/doubleslash/quiz/transport/web/config/CorsConfiguration.java
@@ -1,6 +1,7 @@
 package de.doubleslash.quiz.transport.web.config;
 
 import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,19 +14,13 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfiguration {
 
   @Value("${cors.client.url}")
-  private String allowedOrigin;
+  private List<String> allowedOrigins;
   @Bean
   public CorsFilter corsFilter() {
-    var origins = allowedOrigin.split(",");
-    log.info("Allowed Origin:");
-
-    for(var origin : origins)
-      log.info("ORIGIN: " + origin);
-
     final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     org.springframework.web.cors.CorsConfiguration config = new org.springframework.web.cors.CorsConfiguration();
     config.setAllowCredentials(true);
-    config.setAllowedOrigins(Arrays.asList(origins));
+    config.setAllowedOrigins(allowedOrigins);
     config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "responseType", "Authorization"));
     config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
     source.registerCorsConfiguration("/**", config);
